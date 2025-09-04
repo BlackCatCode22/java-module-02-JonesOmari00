8dbbcaa8a0122d75fc56db93d8e2660bb704ed9f
Jonesomari00 <jonesomari00@outlook.com>
2025-09-04 10:43:06 -0700

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,11 @@
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+      String[] nbaplayers = {"Kobe" , "Lebron" , "Jordan"};
+    // For nbaplayers array code up nbaplayers
+      for(String nbaplayer : nbaplayers) {
+          System.out.println(nbaplayer);
+
+      }
+    }
+}
